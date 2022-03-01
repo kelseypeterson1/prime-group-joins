7c4791bbaabad49b4a3b402eb350021e5af6d38c
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-01 11:51:06 -0600

    answered base questions 1-8

---
+++ joins-solution.sql
@@ -0,0 +1,47 @@
+--SQL Queries
+
+1. Get all customers and their addresses.
+SELECT *
+FROM "customers"
+JOIN "addresses" ON "addresses"."customer_id" = "customers"."id";
+
+2. Get all orders and their line items (orders, quantity and product).
+SELECT * 
+FROM "orders"
+JOIN "line_items" ON "line_items"."order_id" = "orders"."id";
+
+3. Which warehouses have cheetos?
+SELECT "warehouse"."warehouse" AS "warehouse name"
+FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product"."warehouse_id" = "warehouse"."id"
+JOIN "products" ON "warehouse_product"."product_id" = "products"."id"
+WHERE "products"."description" = 'cheetos';
+
+4. Which warehouses have diet pepsi?
+SELECT "warehouse"."warehouse" AS "warehouse name"
+FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product"."warehouse_id" = "warehouse"."id"
+JOIN "products" ON "warehouse_product"."product_id" = "products"."id"
+WHERE "products"."description" = 'diet pepsi';
+
+5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT "customers"."id", count("orders"."id")
+FROM "customers"
+JOIN "addresses" ON "addresses"."customer_id" = "customers"."id"
+JOIN "orders" ON "addresses"."id" = "orders"."address_id"
+GROUP BY "customers"."id";
+
+6. How many customers do we have?
+SELECT count("customers"."id")
+FROM "customers";
+
+7. How many products do we carry?
+SELECT count("products"."id")
+FROM "products";
+
+8. What is the total available on-hand quantity of diet pepsi?
+SELECT "products"."description", sum("warehouse_product"."on_hand")
+FROM "products"
+JOIN "warehouse_product" ON "products"."id" = "warehouse_product"."product_id"
+WHERE "products"."description" = 'diet pepsi'
+GROUP BY "products"."description";
